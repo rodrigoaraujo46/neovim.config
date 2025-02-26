b81fe1ae4f9c5976d8da6a460000320d134ec1e3
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-02-26 23:19:48 +0000

    My name instead of primes.

--- lua/rax/init.lua
+++ lua/rax/init.lua
@@ -1,6 +1,6 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
-require("theprimeagen.lazy_init")
+require("rax.set")
+require("rax.remap")
+require("rax.lazy_init")
 
 -- DO.not
 -- DO NOT INCLUDE THIS
@@ -14,7 +14,7 @@ require("theprimeagen.lazy_init")
 -- DO.not
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local raxGroup = augroup('rax', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -41,13 +41,13 @@ autocmd('TextYankPost', {
 })
 
 autocmd({ "BufWritePre" }, {
-    group = ThePrimeagenGroup,
+    group = raxGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
 
 autocmd('BufEnter', {
-    group = ThePrimeagenGroup,
+    group = raxGroup,
     callback = function()
         --[[
         if vim.bo.filetype == "zig" then
@@ -61,7 +61,7 @@ autocmd('BufEnter', {
 
 
 autocmd('LspAttach', {
-    group = ThePrimeagenGroup,
+    group = raxGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
--- lua/rax/lazy/fugitive.lua
+++ lua/rax/lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local rax_Fugitive = vim.api.nvim_create_augroup("rax_Fugitive", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+            group = rax_Fugitive,
             pattern = "*",
             callback = function()
                 if vim.bo.ft ~= "fugitive" then
